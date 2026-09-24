d268bd13afc1b1eb47981112503b4b11d5e21564
Ahsan <ahsan.moh426@gmail.com>
2026-09-24 12:11:57 -0400

    USACO Training Pages - crypt1 Pretty easy, might have taken like 30 minutes, just one submission Honestly no notes, just easy, im happy im at this stage i guess. Back in my prime fr, at this point I'd lowk be better off with trying new types of problems(binary string, DP, trees, etc)but idk. Basically good with brute force at this point. New stuff: set, just a set. unordered set, just unordered map but without the values. You know, i didnt even have to debug for this. This was mad easy. My only debugging I did was swtiching to set after i found out unordered set was not compatible with vector.

---
+++ USACO/combo/combo.cpp
@@ -0,0 +1,47 @@
+/*
+ID: ahsan.m1
+TASK: combo
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+int mod(int a, int b);
+
+int main(){
+    //input section
+    ifstream fin ("combo.in");
+    int n; fin >> n;
+    vector<int> john_locke = vector<int>(3);
+    vector<int> master_lock = vector<int>(3);
+    for(auto& fhjfk : john_locke){
+        fin >> fhjfk;
+    }
+    for(auto& fhjfk : master_lock){
+        fin >> fhjfk;
+    }
+    fin.close();
+    //input complete
+
+    //create triples
+    set<vector<int>> possible_triples;
+    for(int i = -2; i<=2;i++){
+        for(int j = -2; j<=2;j++){
+            for(int k = -2; k<=2;k++){
+                possible_triples.insert({mod(john_locke[0]+i, n), mod(john_locke[1]+j, n), mod(john_locke[2]+k, n)});
+                possible_triples.insert({mod(master_lock[0]+i, n), mod(master_lock[1]+j, n), mod(master_lock[2]+k, n)});
+            }
+        }
+    }
+    ofstream fout ("combo.out");
+    fout << possible_triples.size() << endl;
+    fout.close();
+    return 0;
+}
+int mod(int a, int b) {
+    return ((a % b) + b) % b;
+}
